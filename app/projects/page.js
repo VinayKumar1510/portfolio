c0a6de6189f6172d141c8d884eb59910ecbfd1e0
VinayKumar1510 <146341986+VinayKumar1510@users.noreply.github.com>
2025-04-26 12:51:57 +0530

    Update page.js

--- app/projects/page.js
+++ app/projects/page.js
@@ -7,7 +7,8 @@ import Link from 'next/link';
 const projects = [
     { src: "/chatapp.png", text: "A real-time chat application with WebSocket integration.", link: "https://chat-app-seven-red.vercel.app/" },
     { src: "/nopoverty.png", text: "A platform aimed at promoting sustainable development goals.", link: "https://no-poverty-vma5.vercel.app/" },
-    { src: "/vinayblog.png", text: "A personal blogging platform with Markdown support.", link: "https://vinay-blogs.vercel.app/" }
+    { src: "/vinayblog.png", text: "A personal blogging platform with Markdown support.", link: "https://vinay-blogs.vercel.app/" },
+    { src: "/notesapp.png", text: "Create new notes and record your voice easily with this intuitive app.", link: "https://bitnotes-iota.vercel.app/" }
 ];
 
 const Projects = () => {
